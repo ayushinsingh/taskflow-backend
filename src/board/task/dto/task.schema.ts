c8da8ea8e935a3970e5226f694c9ddeabdeb75ab
Ayush Singh <ayushinsingh7@gmail.com>
2026-08-10 13:43:04 +0530

    fixed schema

--- src/board/task/dto/task.schema.ts
+++ src/board/task/dto/task.schema.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const createTaskSchema = z.object({
-  assignedToId: z.uuid().nullable().optional(),
+  assignedToId: z.cuid2().nullable().optional(),
   title: z.string().min(2).max(100),
   description: z.string().default(""),
   priority: z.enum(["HIGH", "MEDIUM", "LOW", "URGENT"]).default("MEDIUM"),
@@ -15,7 +15,7 @@ export const taskParamsSchema = z.object({
 });
 
 export const updateTaskSchema = z.object({
-  assignedToId: z.uuid().nullable().optional(),
+  assignedToId: z.cuid2().nullable().optional(),
   title: z.string().min(2).max(100).optional(),
   description: z.string().default("").optional(),
   priority: z.enum(["HIGH", "MEDIUM", "LOW", "URGENT"]).default("MEDIUM").optional(),
